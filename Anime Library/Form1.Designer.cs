7d8129d16816408b3fede5026c0c041c5aa39e3d
Levi <levijoshuagould@gmail.com>
2020-07-19 18:51:50 +0200

    Updated.

---
+++ Anime Library/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Anime_Library
+{
+    partial class AnimeLibrary
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AnimeLibrary));
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.selectedAnimeLabel = new System.Windows.Forms.Label();
+            this.episodeComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 35);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(530, 300);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Search:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(62, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(280, 20);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(388, 343);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 20);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(257, 349);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Episode:";
+            // 
+            // selectedAnimeLabel
+            // 
+            this.selectedAnimeLabel.AutoSize = true;
+            this.selectedAnimeLabel.ForeColor = System.Drawing.Color.White;
+            this.selectedAnimeLabel.Location = new System.Drawing.Point(9, 349);
+            this.selectedAnimeLabel.Name = "selectedAnimeLabel";
+            this.selectedAnimeLabel.Size = new System.Drawing.Size(42, 13);
+            this.selectedAnimeLabel.TabIndex = 16;
+            this.selectedAnimeLabel.Text = "Anime: ";
+            // 
+            // episodeComboBox
+            // 
+            this.episodeComboBox.FormattingEnabled = true;
+            this.episodeComboBox.Location = new System.Drawing.Point(311, 343);
+            this.episodeComboBox.Name = "episodeComboBox";
+            this.episodeComboBox.Size = new System.Drawing.Size(59, 21);
+            this.episodeComboBox.TabIndex = 17;
+            this.episodeComboBox.SelectedIndexChanged += new System.EventHandler(this.episodeComboBox_SelectedIndexChanged);
+            // 
+            // AnimeLibrary
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(554, 376);
+            this.Controls.Add(this.episodeComboBox);
+            this.Controls.Add(this.selectedAnimeLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(574, 419);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(574, 419);
+            this.Name = "AnimeLibrary";
+            this.Text = "Anime Library";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label selectedAnimeLabel;
+        private System.Windows.Forms.ComboBox episodeComboBox;
+    }
+}
+
---
+++ Anime Library/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Anime_Library
+{
+    public partial class AnimeLibrary : Form
+    {
+        string selectedAnime = "";
+        string selectedEpisode = "0";
+        public AnimeLibrary()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            flowLayoutPanel1.Controls.Clear();
+            using (WebClient client = new WebClient())
+            {
+                string htmlCode = client.DownloadString("https://ajax.apimovie.xyz/site/loadAjaxSearch?keyword=" + textBox1.Text);
+
+                string[] words = htmlCode.Split(' ');
+                ToolTip current = null;
+
+                foreach (var word in words)
+                {
+                    if (word.Contains("category"))
+                    {
+                        try
+                        {
+                            int pFrom = word.IndexOf("href=\\\"category\\/") + "href=\\\"category\\/".Length;
+                            int pTo = word.LastIndexOf("\\\"");
+
+                            string result = word.Substring(pFrom, pTo - pFrom);
+                            result = result.Replace("-", " ");
+                            result = UpperCaseFirstLetter(result);
+
+
+                            ToolTip toolTip = new ToolTip();
+                            toolTip.InitialDelay = 150;
+                            toolTip.ReshowDelay = 500;
+                            toolTip.ShowAlways = true;
+                            toolTip.Tag = result;
+                            current = toolTip;
+                        }
+                        catch { }
+                    }
+
+                    if (word.Contains("cdnimg.xyz"))
+                    {
+                        try
+                        {
+                            string animePicture;
+                            animePicture = word.Replace("\\", "");
+                            int first = animePicture.IndexOf("\"") + 1;
+                            int second = animePicture.IndexOf(")") - 6;
+                            string result = animePicture.Substring(first, second);
+
+                            PictureBox imageControl = new PictureBox();
+                            Size size = new Size(100, 150);
+                            imageControl.MinimumSize = size;
+                            imageControl.MaximumSize = size;
+                            imageControl.SizeMode = PictureBoxSizeMode.StretchImage;
+                            imageControl.Load(result);
+                            flowLayoutPanel1.Controls.Add(imageControl);
+                            current.SetToolTip(imageControl, current.Tag.ToString());
+                            string animeName = current.Tag.ToString();
+
+                            imageControl.MouseClick += new MouseEventHandler((s, e1) =>
+                            {
+                                selectedAnimeLabel.Text = "Anime: " + animeName;
+                                selectedAnime = animeName;
+
+                                string animeCode = animeName;
+                                animeCode = animeCode.Replace(" ", "-");
+                                string htmlCode2 = client.DownloadString("https://www.gogoanime.io/category/" + animeCode);
+
+                                string[] newWords = htmlCode2.Split(' ');
+
+                                Clipboard.SetText(htmlCode2);
+                                int times = 0;
+                                foreach (var newWord in newWords)
+                                {
+                                    times++;
+                                    if (newWord.Contains("ep_end"))
+                                    {
+                                        string variable = newWords[times + 1];
+                                        int first2 = variable.IndexOf("'") + 1;
+                                        int second2 = variable.IndexOf("'", first2) - 1;
+                                        string result2 = variable.Substring(first2, second2);
+
+                                        int epCount = int.Parse(result2);
+
+                                        episodeComboBox.Items.Clear();
+                                        for (int ep = 1; ep < epCount+1; ep++)
+                                        {
+                                            episodeComboBox.Items.Add(ep.ToString());
+                                            episodeComboBox.SelectedIndex = 0;
+                                        }
+
+                                        break;
+                                    }
+                                }
+
+
+                            });
+                        }
+                        catch { }
+                    }
+
+                }
+            }
+        }
+
+
+        public string UpperCaseFirstLetter(string YourLowerCaseWord)
+        {
+            if (string.IsNullOrEmpty(YourLowerCaseWord))
+                return string.Empty;
+
+            return char.ToUpper(YourLowerCaseWord[0]) + YourLowerCaseWord.Substring(1);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (WebClient client = new WebClient())
+            {
+                string animeCode = selectedAnime.Replace(" ", "-");
+                string newPage = client.DownloadString("https://www.gogoanime.io/" + animeCode + "-episode-" + selectedEpisode);
+                var linkParser = new Regex(@"\b(?:https?://|www\.)\S+\b", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                foreach (Match m in linkParser.Matches(newPage))
+                    if (m.Value.Contains("vidstreaming.io"))
+                    {
+                        string url = m.Value.Replace("download", "streaming.php");
+                        int index = url.IndexOf("=&typesub");
+                        if (index > 0)
+                            url = url.Substring(0, index);
+
+                        System.Diagnostics.Process.Start(url);
+                    }
+
+            }
+        }
+
+        private void episodeComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedEpisode = episodeComboBox.SelectedItem.ToString();
+        }
+    }
+}
